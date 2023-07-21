4d2174014dcae2a2103f47f86546afb6c2f3eca9
Vasile Midrigan <111rigan111@gmail.com>
2023-07-21 22:37:41 +0100

    Introduction to JavaScript

---
+++ introduction_to_javascript/script.js
@@ -0,0 +1,59 @@
+/*
+
+------------
+ JavaScript
+------------
+
+ The intention of creating js was to make webpages alive.
+
+ The programs in JavaScript are called scripts.
+
+ JavaScript can run in browser, on server and on any other device
+ that has tha JavaScript engine.
+
+ Different browsers have different JavaScript engines, V9 in Chrome, 
+ SpiderMonkey in Mozilla, Chakra for IE, etc. 
+
+ -----------------------------
+ How do Engine work basically? 
+ -----------------------------
+ 1. The engine reads the script
+ 2. Converts the script into machine code
+ 3. Machine code runs
+
+ The engine applies optimizations at each step of the process. 
+
+ ---------------------------------
+ In browser JavaScript is able to: 
+ ---------------------------------
+ 1. Manipulate the content of the webpage
+ 2. React to user actions
+ 3. Send requests over the network to remote servers
+ 4. Get and set cookies, ask questions to the visitors.
+ 5. Remember the data on the client side. 
+ 6. And many other things. 
+
+ ------------------------------------
+ In browser JavaScript isn't able to: 
+ ------------------------------------
+ In browser JavaScript abilities are limited in order to protect the user's 
+ safety.
+ - no access to the OS files and functions, with a few little exceptions and
+   permitions.
+ - one page doesn't have the access to another one (Same Origin Policy). To 
+   break the rule, both pages must agree for data exchange. 
+ - JS can communicate to the server where the current page comes from, but 
+   it cannot communicate with another sites or domains, it requires explicit
+   agreements. 
+
+   Such limitations are specific for browser envinronments but not on a server 
+   for example. 
+
+ -----------------------------
+ What makes JavaScript unique?
+ -----------------------------
+ - Full integration with HTML/CSS
+ - Simple things are done simply
+ - Suported by all major browsers and enabled by default
+
+ */
