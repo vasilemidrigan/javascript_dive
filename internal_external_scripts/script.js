0c36ae95fa9be4d1f7a3d6d0295bb20e79bad91a
Vasile Midrigan <111rigan111@gmail.com>
2023-07-22 09:21:34 +0100

    Internar and External scripts

---
+++ internal_external_scripts/script.js
@@ -0,0 +1,18 @@
+/*
+  -----------
+  Hello World
+  -----------
+    
+  <script> has type attribute, it was used in html4 for spe-
+  cifying the type of document: 
+  <script type="text/javascript">
+  
+  but nowadays it is used to specify that the script is a 
+  module:  
+  <script type="module">
+  
+  
+
+*/
+
+console.log("hello world!");
