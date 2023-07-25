57ff6afecd10d195c9191ce8aef19cf6f8ba9b70
Vasile Midrigan <111rigan111@gmail.com>
2023-07-25 10:49:13 +0100

    comments

---
+++ comments/script.js
@@ -0,0 +1,31 @@
+/*
+  --------
+  Comments
+  --------
+  
+  In a good code, the amount of comments should be minimal, the code 
+  should be easy to understand without comments. 
+
+  If there is a lot of comments, then maybe we need to rewrite the code?
+
+  The code should be self-descriptive.
+
+  Good comments usually are those that give a bird eye view of the code, 
+  the control flow in various situations, etc. 
+
+  There is a tool called JSDoc for documenting functions: usage, parameters, 
+  returned value, such comments allows us to understand the function 
+  without looking into it. 
+
+  Sometimes you fix an older solution, because you think you have now a 
+  better one, but then you realize that the older was there for a reason, 
+  and now you reverse the solution to the older one. The time was wasted. 
+  So writing comments that explain the SOLUTION are very important, in 
+  order to understand WHY was specifically this solution implemented and
+  not another one that seems more reasonable at the moment. 
+
+  So we comment: overall architecture, high-level view, function usage, 
+  important solutions (especially when not obvious)
+
+  Avoid code that tell how code works, and what it does. 
+*/
