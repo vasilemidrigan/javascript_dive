8320fb67fdaff38c7c6126c678a618fdb706e510
Vasile Midrigan <111rigan111@gmail.com>
2023-08-12 13:46:52 +0100

    mouse events

--- BROWSER/UI_Events/Mouse_Events/script.js
+++ BROWSER/UI_Events/Mouse_Events/script.js
@@ -1,6 +1,19 @@
 /* 
   Mouse Events
   ------------
-
   
+  A user action, may trigger multiple events, for example when clicking a
+  mouse button, we have mousedown, then mouseup and then click event. 
+
+  We can work with the mouse cursor coordinates (*)
+
+  Preventing selecting text, copying text, etc, by using mouse events.  
 */
+
+// *
+
+let area = document.getElementById("area");
+
+area.addEventListener("mousemove", function (e) {
+  this.textContent = `x: ${e.clientX}, y: ${e.clientY}`;
+});
